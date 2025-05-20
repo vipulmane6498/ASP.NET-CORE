9e94c3e3d27cfc618e248b31bdb8f7b2535a1c5a
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-05-21 00:12:57 +0530

    Add files via upload

---
+++ WebAPIController_ProjectSolution/WebAPIController_Project/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIController_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public String Method1()
+        {
+            return "Hello World !!";
+        }
+    }
+}
