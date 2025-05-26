25abed31adbf4315edb5d19d0417bb218fee5350
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-05-27 00:30:37 +0530

    Add files via upload

---
+++ 3. Middleware [MVC and Web API]/2. CustomMiddlewareExample/CustomMiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CustomMiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            //Logic -> whatever you want to add
+            await context.Response.WriteAsync("My Custom Middleware class started...");
+            await next(context); //calling next middleware once it gets execute below statement will execute
+           
+            await context.Response.WriteAsync("End");
+        }
+    }
+}
+
