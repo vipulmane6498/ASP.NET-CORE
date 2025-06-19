1264813b3bd79b961e145118f0816fb6bbba8c00
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-20 00:05:52 +0530

    Add files via upload

---
+++ 06. Controllers and IActionResult/03. ContentResult/ContentResultExample/ContentResultExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContentResultExample.Controllers
+{
+
+    
+    public class HomeController : Controller //To use inbuilt methods of controller class then inherit with the same
+    {
+
+
+        [Route("/home")]
+        public ContentResult Index()
+        {
+
+            //ContentResult() -> to return altered response 
+            //return new ContentResult()
+            //{
+            //    Content = "hello there !",
+            //    ContentType = "text/plain"
+            //};
+
+            //Content() method -> it is predefined method in Controller class which we have used for inheritance here
+            return Content("<h1>Hello There !</h1>  <h4>Hello from Index</h4>", "text/html");
+                
+        }
+    }
+}
