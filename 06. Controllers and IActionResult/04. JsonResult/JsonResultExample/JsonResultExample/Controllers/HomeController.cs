4f7e60c94e6a96924bed44ebb8c6271786fee75f
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-20 13:41:51 +0530

    Add files via upload

---
+++ 06. Controllers and IActionResult/04. JsonResult/JsonResultExample/JsonResultExample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JsonResultExample.Models;
+
+namespace JsonResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        [Route("home")]
+        [Route("/")]
+        public string Index()
+        {
+            return "HomePage !";
+        }
+        
+
+        [Route("person")]
+        public JsonResult Person()
+        {
+
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                firstName = "James",
+                lastName = "Bond",
+                age = 34
+
+            };
+            return new JsonResult(person);
+
+        }
+    }
+}
---
+++ 06. Controllers and IActionResult/04. JsonResult/JsonResultExample/JsonResultExample/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace JsonResultExample.Models
+{
+    public class Person
+    {
+        //Guid is a data type for generate unique numbers which are unlimited
+        public Guid Id { get; set; } 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public int age { get; set; }
+
+
+    }
+}
