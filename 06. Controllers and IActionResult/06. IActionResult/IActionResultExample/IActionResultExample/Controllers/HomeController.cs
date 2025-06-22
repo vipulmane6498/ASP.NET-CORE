7458d98da46fc08015f16becdda5f410128d1c37
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-22 15:13:46 +0530

    Add files via upload

---
+++ 06. Controllers and IActionResult/06. IActionResult/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("book")]
+        public IActionResult Index()
+        {
+            //BookId should be applied
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                Response.StatusCode = 400;
+                return Content("Bookid is not supplied");
+            }
+
+            //bookid cannot be null
+            if ( string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                Response.StatusCode = 400;
+                return Content("Bookid cannot be empty !");
+            }
+
+            //1st Recieve bookid and store in variable
+            //2nd  check Book id should be between 1 to 1000
+            int bookid = Convert.ToInt32(HttpContext.Request.Query["bookid"]);
+            if(bookid <= 0)
+            {
+                Response.StatusCode = 400;
+                return Content("Book id can't be less than or equal to zero");
+            }
+            if (bookid > 1000)
+            {
+                Response.StatusCode = 400;
+                return Content("Book id can't be greater than 1000");
+            }
+
+            if (Convert.ToBoolean(Request.Query["isloggedin"]) == false){
+
+                Response.StatusCode = 401;
+                return Content("User must be authenticated");
+            }
+
+            return File("/Hello.pdf", "application/pdf");
+        }
+    }
+}
