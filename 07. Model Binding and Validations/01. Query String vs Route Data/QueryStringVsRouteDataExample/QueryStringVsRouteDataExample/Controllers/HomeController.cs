c795f3253d974671483f60c3c1831e7d39359bc4
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-26 20:34:00 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/01. Query String vs Route Data/QueryStringVsRouteDataExample/QueryStringVsRouteDataExample/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QueryStringVsRouteDataExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //http://localhost:5125/bookstore?bookid=20&isloggedin=true
+        [Route("bookstore/{bookid?}/{isloggedin?}")]      
+        public IActionResult Index(int? bookid, bool? isloggedin)
+        {
+            //BookId should be applied
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Bookid is not supplied");//shorthand method -> return method with content.
+            }
+
+            //As you are recieving the value in method paramter you dont need to receive it using Request.Query like below
+            //int bookid = Convert.ToInt32(HttpContext.Request.Query["bookid"]);
+
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+            if (isloggedin == false)
+            {
+                return Unauthorized("User must be authorized");
+            }
+
+            //when above all condtion gets false true output
+            return Content($"Book id: {bookid}", "text/plain"); 
+
+        }
+    }
+
+}
---
+++ 07. Model Binding and Validations/02. FromQuery and FromRoute/FromQueryAndFromRouteExample/FromQueryAndFromRouteExample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FromQueryAndFromRouteExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        //http://localhost:5125/bookstore?bookid=20&isloggedin=true
+
+        public IActionResult Index([FromQuery]int? bookid, [FromQuery]bool? isloggedin) //[FromQuery]
+        //public IActionResult Index([FromRoute]int? bookid, [FromRoute]bool? isloggedin)  //[FromRoute]
+        {
+            //BookId should be applied
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Bookid is not supplied");//shorthand method -> return method with content.
+            }
+
+            
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            if (isloggedin == false)
+            {
+                return Unauthorized("User must be authorized");
+            }
+
+            //when above all condtion gets false true output
+            return Content($"Book id: {bookid}", "text/plain");
+        }
+    }
+}
