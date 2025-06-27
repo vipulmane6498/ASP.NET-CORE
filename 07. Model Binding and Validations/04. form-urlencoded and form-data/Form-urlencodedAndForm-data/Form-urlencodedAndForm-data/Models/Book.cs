c3bd39b7fdfd3847cb2a60bf3849829a37a16766
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-27 22:54:54 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/04. form-urlencoded and form-data/Form-urlencodedAndForm-data/Form-urlencodedAndForm-data/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Form_urlencodedAndForm_data.Models
+{
+    public class Book
+    {
+
+        public int bookid { get; set; }
+
+        public string author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book id is: {bookid} and Auther is: {author}";
+        }
+    }
+}
