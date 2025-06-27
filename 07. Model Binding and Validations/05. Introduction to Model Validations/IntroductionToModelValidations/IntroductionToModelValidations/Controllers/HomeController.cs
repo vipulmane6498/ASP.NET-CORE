8370117c670490c33a9a4498ae105d4e9351e5fb
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-27 22:55:41 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/05. Introduction to Model Validations/IntroductionToModelValidations/IntroductionToModelValidations/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntroductionToModelValidations.Models;
+namespace IntroductionToModelValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            return Content($"{person}");
+        }
+    }
+}
---
+++ 07. Model Binding and Validations/05. Introduction to Model Validations/IntroductionToModelValidations/IntroductionToModelValidations/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroductionToModelValidations.Models
+{
+    public class Person
+    {
+        [Required] //this attribute -> property value should not be null/empty should should be some value
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - Person name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, Confirm Password: {ConfirmPassword}, Price: {Price}";
+        }
+
+    }
+}
