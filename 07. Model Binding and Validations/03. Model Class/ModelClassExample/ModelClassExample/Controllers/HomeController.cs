fc982b4662a3657c72a9dc0911817ce78f6e16b9
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-27 13:02:07 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/03. Model Class/ModelClassExample/ModelClassExample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelClassExample.Models;
+
+namespace ModelClassExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        //http://localhost:5080/bookstore/1/true?bookid=10&isloggedin=false&author=vipul
+        public IActionResult Index([FromQuery] int? bookid, [FromRoute] bool? isloggedin, Book book) 
+                                                                                //[FromQuery]Book book
+        {
+            
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Bookid is not supplied");    
+            }
+
+
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            if (isloggedin == false)
+            {
+                return Unauthorized("User must be authorized");
+            }
+
+         
+
+            //when above all condtion gets false true output
+            return Content($"Book id: {bookid}, isloggedin: {isloggedin} and Author is: {book.author}", "text/plain");
+        }
+    }
+}
---
+++ 07. Model Binding and Validations/03. Model Class/ModelClassExample/ModelClassExample/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelClassExample.Models
+{
+
+    public class Book
+    {
+        [FromQuery]
+        public int? bookid { get; set; } //bookid will take value from Quyerstring as
+                                         //we have used [FromQuery]
+
+        public string? author { get; set; } //author will take value from Routedata
+                                            //but in url we mention value in QueryString
+
+
+        //ToString method is used to print the properite values
+        public override string ToString()
+        {
+            return $"book id: {bookid} and Author is: {author}";
+        }
+    }
+}
