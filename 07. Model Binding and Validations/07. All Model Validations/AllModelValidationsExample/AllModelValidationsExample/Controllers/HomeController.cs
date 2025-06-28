24538967d44cd19016fb4c4d4830f5e19f386a7e
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-29 01:59:37 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/07. All Model Validations/AllModelValidationsExample/AllModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+    using AllModelValidationsExample.Models;
+    namespace AllModelValidationsExample.Controllers
+    {
+        public class HomeController : Controller
+        {
+
+            [Route("register")]
+            public IActionResult Index(Person person)
+            {
+                if (!ModelState.IsValid)
+                {
+
+                string error=  string.Join("\n",  
+                    ModelState.Values.SelectMany(value => 
+                value.Errors).Select(err=>err.ErrorMessage));
+
+                return Content(error);
+                }
+
+                return Content($"{person}");
+            }
+        }
+    }
