a3ec05821be3cf26f1b7f3474b67120dfc123b6d
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-06-30 23:18:11 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/10. IValidatableObject/IValidatableObjectExample/IValidatableObjectExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using IValidatableObjectExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IValidatableObjectExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                   
+                string error = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage)); 
+                //if Format not worked use "Join"
+
+                return BadRequest(error);
+            }
+            return Content($"{person}");
+        }
+    }
+}
