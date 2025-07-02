86bd6ce773c7efff7ec56853cf2265b33c048a3a
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-02 12:58:44 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/11. Bind and BindNever/BindAndNeverExample/BindAndNeverExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using BindAndNeverExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BindAndNeverExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        [Route("register")]
+        //[Bind(nameof(person.PersonName), "Age", "DateOfBirth")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+              string error = string.Join("\n", ModelState.Values.SelectMany(
+                  value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(error);   
+            }
+            return Content($"{person}");
+        }
+    }
+}
