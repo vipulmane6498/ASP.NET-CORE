f4b3a22cdb5458cc7460882d3a8f46bf127ff9fa
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-03 15:47:18 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/12. FromBody/FromBodyExample/FromBodyExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using FromBodyExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromBodyExample
+    {
+    public class HomeController : Controller
+    {
+
+
+        [Route("register")]
+        public IActionResult Index([FromBody]Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+              string error = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select
+                  (err => err.ErrorMessage));
+
+                return BadRequest(error);   
+            }
+            return Content($"{person}");
+        }
+    }
+}
