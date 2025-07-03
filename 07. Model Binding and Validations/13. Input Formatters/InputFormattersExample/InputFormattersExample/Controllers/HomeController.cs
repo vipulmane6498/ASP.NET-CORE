de0fd0b0d864eb0b39df4072f798da36a8d93ac5
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-04 00:45:51 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/13. Input Formatters/InputFormattersExample/InputFormattersExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using InputFormattersExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InputFormattersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        [Route("register")]
+        //[Bind(nameof(person.PersonName), "Age", "DateOfBirth")]
+        public IActionResult Index([FromBody]Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+              string error = string.Join("\n", ModelState.Values.SelectMany(
+                  value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(error);   
+            }
+            return Content($"{person}");
+        }
+    }
+}
