bee3b1f53fb2d7401be66396a431a9824096ed07
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-06 20:36:20 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/14. Custom Model Binders/CustomModelBindersExample/CustomModelBindersExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CustomModelBindersExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using CustomModelBindersExample.CustomModelBinders;
+
+namespace CustomModelBindersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        [Route("register")]
+        //[Bind(nameof(person.PersonName), "Age", "DateOfBirth")]
+        public IActionResult Index([FromBody] [ModelBinder(BinderType = typeof(PersonModelBinder))] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+              string error = string.Join("\n", ModelState.Values.SelectMany(
+                  value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(error);   
+            }
+            return Content($"{person}");
+        }
+    }
+}
