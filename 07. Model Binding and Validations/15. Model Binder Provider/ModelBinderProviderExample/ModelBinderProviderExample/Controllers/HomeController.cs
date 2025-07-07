f9595b7e1e1e59559a76a310ae21392dc68dcf7d
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-07 20:09:37 +0530

    Add files via upload

---
+++ 07. Model Binding and Validations/15. Model Binder Provider/ModelBinderProviderExample/ModelBinderProviderExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ModelBinderProviderExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinderProviderExample.CustomModelBinders;
+
+namespace ModelBinderProviderExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        [Route("register")]
+        //[ModelBinder(BinderType = typeof(PersonModelBinder))]
+        public IActionResult Index( Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+              string error = string.Join("\n", ModelState.Values.SelectMany(
+                  value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(error);   
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ 07. Model Binding and Validations/15. Model Binder Provider/ModelBinderProviderExample/ModelBinderProviderExample/CustomModelBinders/PersonBinderProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using ModelBinderProviderExample.Models;
+using ModelBinderProviderExample.CustomModelBinders;
+namespace ModelBinderProviderExample.CustomModelBinders
+{
+    public class PersonBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+
+                        if(context.Metadata.ModelType == typeof(Person))
+            {
+                return new BinderTypeModelBinder(typeof(PersonModelBinder));
+            }
+            return null;
+        }
+    }
+}
