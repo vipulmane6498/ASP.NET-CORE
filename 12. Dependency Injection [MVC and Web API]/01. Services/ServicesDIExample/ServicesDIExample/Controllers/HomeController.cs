27dfaa3c7972ff44c57e43f2472f285a5fe82a0a
Vipul Mane <113690721+vipulmane6498@users.noreply.github.com>
2025-07-10 01:20:11 +0530

    Add files via upload

---
+++ 12. Dependency Injection [MVC and Web API]/01. Services/ServicesDIExample/ServicesDIExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ServicesDIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly CitiesService _citiesService;
+
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+           List<string?> cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
